a646b9b0cb9031c5184f0aa55d8127a7d8416c63
ced-77 <=>
2017-01-24 00:29:30 +0100

    ajout du fichier scritp.js pour la gestion du menu burger avec detection du click sur fa.bars

---
+++ script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+	/*
+		Mise en place du burger menu avec une 
+		detection de click sur le fa.bar
+
+		Le click devra ouvrir le menu en le fesant descendre
+		ou remonter pour le fermer
+
+		Le click devra changer la bar en cross ou time pour la fermeture
+
+	 */
+	
+
+
+	// detection du click sur fa.bar
+	
+		$(".fa-bars").on('click', function(event) {
+			// annulation de la reaction par default
+			event.preventDefault();
+
+			// 
+			console.log('on appuyer sur fa.bars');
+
+
+
+		// fin de la detection du click sur fa.bars
+		})
+
+
+
+
+// fin du script jQuery
+})
